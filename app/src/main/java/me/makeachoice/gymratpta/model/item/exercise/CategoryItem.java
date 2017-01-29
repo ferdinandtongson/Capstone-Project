96c5448499c8745b3b1eda7c6c15da0fcda4ef41
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-01-28 18:54:46 -0800

    Add CategoryItem and ExerciseItem classes

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/exercise/CategoryItem.java
@@ -0,0 +1,13 @@
+package me.makeachoice.gymratpta.model.item.exercise;
+
+/**
+ * Created by Usuario on 12/24/2016.
+ */
+
+public class CategoryItem {
+    public String key;
+    public String categoryName;
+
+    public CategoryItem(){}
+
+}
---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/exercise/ExerciseItem.java
@@ -0,0 +1,16 @@
+package me.makeachoice.gymratpta.model.item.exercise;
+
+/**
+ * Created by Usuario on 12/24/2016.
+ */
+
+public class ExerciseItem {
+    public String exerciseKey;
+    public String exerciseName;
+    public String exerciseCategory;
+    public String recordPrimary;
+    public String recordSecondary;
+
+    public ExerciseItem(){}
+
+}
