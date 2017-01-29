ce8e14f223b1707e03e87104d28d96b8e620f56b
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-01-28 20:14:35 -0800

    Add StubMaid and layout resource for scaffolding purposes

---
+++ app/src/main/java/me/makeachoice/gymratpta/controller/viewside/maid/StubMaid.java
@@ -0,0 +1,137 @@
+package me.makeachoice.gymratpta.controller.viewside.maid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import me.makeachoice.gymratpta.R;
+import me.makeachoice.gymratpta.controller.manager.MaidRegistry;
+import me.makeachoice.gymratpta.model.item.exercise.CategoryItem;
+import me.makeachoice.gymratpta.view.fragment.BasicFragment;
+import me.makeachoice.library.android.base.controller.viewside.maid.MyMaid;
+import me.makeachoice.library.android.base.view.activity.MyActivity;
+
+/**************************************************************************************************/
+/*
+ * TODO - add description
+ * Variables from MyMaid:
+ *      mMaidKey - key string of instance Maid
+ *      int mLayoutId - resource id number of fragment layout
+ *      View mLayout - fragment layout view holding the child views
+ *      MyFragment mFragment - fragment being maintained by the Maid
+ *
+ * Methods from MyMaid:
+ *      void activityCreated() - called when Activity.onCreate() completed
+ *      void destroyView() - called when fragment is being removed
+ *      void detach() - called when fragment is being disassociated from Activity
+ *      void saveInstanceState(Bundle) - called before onDestroy( ), save state to bundle
+ *      String getKey() - get maid key value
+ *      MyFragment getFragment() - get fragment maintained by maid
+ */
+
+/**************************************************************************************************/
+
+public class StubMaid extends MyMaid implements BasicFragment.Bridge{
+
+/**************************************************************************************************/
+/*
+ * Class Variables
+ */
+/**************************************************************************************************/
+
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * ExerciseMaid - constructor
+ */
+/**************************************************************************************************/
+    /*
+     * ExerciseMaid(...) - constructor
+     */
+    public StubMaid(String maidKey, int layoutId){
+        //fragment layout id number
+        mLayoutId = layoutId;
+
+        //create fragment
+        mFragment = new BasicFragment();
+
+        //attach maid key to fragment
+        mFragment.setMaidKey(maidKey);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ *  MyMaid Implementation
+ *      View createView(LayoutInflater,ViewGroup,Bundle) - called by Fragment onCreateView()
+ *      void createActivity(Bundle) - is called by Fragment onCreateActivity(...)
+ *      void detach() - fragment is being disassociated from activity
+ */
+/**************************************************************************************************/
+    /*
+     * View createView(LayoutInflater, ViewGroup, Bundle) - is called by Fragment when onCreateView(...)
+     * is called in that class. Prepares the Fragment View to be presented.
+     */
+    public View createView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState){
+        //inflate fragment layout
+        mLayout = inflater.inflate(mLayoutId, container, false);
+
+        //return fragment
+        return mLayout;
+    }
+
+    /*
+     * void createActivity(Bundle) - is called by Fragment when onCreateActivity(...) is called in
+     * that class. Sets child views in fragment before being seen by the user
+     * @param bundle - saved instance states
+     */
+    @Override
+    public void activityCreated(Bundle bundle){
+        super.activityCreated(bundle);
+
+        //prepare fragment components
+        prepareFragment();
+    }
+
+    /*
+     * void detach() - fragment is being disassociated from activity
+     */
+    @Override
+    public void detach(){
+        //fragment is being disassociated from Activity
+        super.detach();
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Class Methods:
+ *      void prepareFragment(View) - prepare components and data to be displayed by fragment
+ *      void initializeRecycler() - initialize recycler to display exercise items
+ *      EditAddDialog initializeDialog(...) - create exercise edit/add dialog
+ */
+/**************************************************************************************************/
+    /*
+     * void prepareFragment(View) - prepare components and data to be displayed by fragment
+     */
+    private void prepareFragment(){
+        MyActivity activity = (MyActivity)mFragment.getActivity();
+
+        TextView txtTitle = (TextView)activity.findViewById(R.id.stub_txtTitle);
+        txtTitle.setText("StubMaid");
+
+    }
+
+
+/**************************************************************************************************/
+
+}
