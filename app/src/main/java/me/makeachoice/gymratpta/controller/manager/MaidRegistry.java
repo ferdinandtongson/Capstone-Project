f8e296c4ed976348b3f65c84eb6218e29477973a
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-01-30 23:17:09 -0800

    Add initializeDayViewPagerMaid() method and change method names related to Appointment Screen

--- app/src/main/java/me/makeachoice/gymratpta/controller/manager/MaidRegistry.java
+++ app/src/main/java/me/makeachoice/gymratpta/controller/manager/MaidRegistry.java
@@ -1,16 +1,16 @@
 package me.makeachoice.gymratpta.controller.manager;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 
 import me.makeachoice.gymratpta.R;
 import me.makeachoice.gymratpta.controller.viewside.maid.StubMaid;
-import me.makeachoice.gymratpta.controller.viewside.maid.appointment.DayPageMaid;
+import me.makeachoice.gymratpta.controller.viewside.maid.appointment.DayMaid;
+import me.makeachoice.gymratpta.controller.viewside.maid.appointment.DayViewPagerMaid;
 import me.makeachoice.gymratpta.controller.viewside.maid.appointment.WeekPageMaid;
 import me.makeachoice.gymratpta.controller.viewside.maid.exercise.ExerciseMaid;
 import me.makeachoice.gymratpta.controller.viewside.maid.exercise.ExerciseViewPagerMaid;
 import me.makeachoice.gymratpta.controller.viewside.maid.exercise.RoutineMaid;
+import me.makeachoice.gymratpta.model.item.ClientCardItem;
 import me.makeachoice.gymratpta.model.item.exercise.ExerciseItem;
 
 /**************************************************************************************************/
@@ -34,8 +34,9 @@ public class MaidRegistry extends MyMaidRegistry {
     public static final String MAID_ROUTINE = "RoutineMaid";
     public static final String MAID_EXERCISE_VP = "ExerciseViewPagerMaid";
 
-    public static final String MAID_DAY_PAGE = "DayPageMaid";
-    public static final String MAID_WEEK_PAGE = "WeekPageMaid";
+    public static final String MAID_DAY = "DayMaid";
+    public static final String MAID_WEEK = "WeekPageMaid";
+    public static final String MAID_DAY_VP = "DayViewPagerMaid";
 
     public static final String MAID_STUB = "StubMaid";
 
@@ -102,15 +103,23 @@ public class MaidRegistry extends MyMaidRegistry {
  */
 /**************************************************************************************************/
 
-    public void initializeDayPageMaid(String maidKey, int layoutId){
+    public void initializeDayMaid(String maidKey, int layoutId){
         //create maid
-        DayPageMaid maid = new DayPageMaid(maidKey, layoutId);
+        DayMaid maid = new DayMaid(maidKey, layoutId);
 
         //register maid
         registerMaid(maidKey, maid);
     }
 
-    public void initializeWeekPageMaid(String maidKey, int layoutId){
+    public void initializeDayViewPagerMaid(String maidKey, int layoutId, ArrayList<ClientCardItem> clients){
+        //create maid
+        DayViewPagerMaid maid = new DayViewPagerMaid(maidKey, layoutId, clients);
+
+        //register maid
+        registerMaid(maidKey, maid);
+    }
+
+    public void initializeWeekMaid(String maidKey, int layoutId){
         //create maid
         WeekPageMaid maid = new WeekPageMaid(maidKey, layoutId);
 
