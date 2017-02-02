af546b77f94cd9ef32076cbb9dd19a36c59b578c
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-02 13:00:28 -0800

    Add UserItem class to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/UserItem.java
@@ -0,0 +1,14 @@
+package me.makeachoice.gymratpta.model.item;
+
+/**
+ * Created by Usuario on 2/2/2017.
+ */
+
+public class UserItem {
+
+    public String uid;
+    public String userName;
+    public String email;
+    public String status;
+
+}
