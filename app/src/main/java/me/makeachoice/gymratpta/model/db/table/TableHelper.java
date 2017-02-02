84d702213e6589c214a557e7a81c262779e699fa
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-02 13:01:07 -0800

    Add database client table string

--- app/src/main/java/me/makeachoice/gymratpta/model/db/table/TableHelper.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/db/table/TableHelper.java
@@ -18,4 +18,13 @@ public class TableHelper {
             Contractor.UserEntry.COLUMN_USER_PHOTO+ " TEXT, " +
             Contractor.UserEntry.COLUMN_USER_STATUS + " TEXT NOT NULL);";
 
+    public final static String SQL_CREATE_CLIENT_TABLE = "CREATE TABLE " + Contractor.ClientEntry.TABLE_NAME + " (" +
+            Contractor.ClientEntry._ID + " INTEGER PRIMARY KEY," +
+            Contractor.ClientEntry.COLUMN_UID + " TEXT NOT NULL, " +
+            Contractor.ClientEntry.COLUMN_FKEY + " TEXT UNIQUE NOT NULL, " +
+            Contractor.ClientEntry.COLUMN_CONTACTS_ID + " TEXT NOT NULL, " +
+            Contractor.ClientEntry.COLUMN_CLIENT_NAME+ " TEXT NOT NULL, " +
+            Contractor.ClientEntry.COLUMN_CLIENT_STATUS + " TEXT NOT NULL);";
+
+
 }
