2c99d397963934926d63bec10299c90ff15478eb
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-03 18:09:26 -0800

    Add phone and email projections and indices

--- app/src/main/java/me/makeachoice/gymratpta/model/contract/contacts/ContactsColumns.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/contacts/ContactsColumns.java
@@ -2,25 +2,29 @@ package me.makeachoice.gymratpta.model.contract.contacts;
 
 import android.provider.ContactsContract;
 
-import static me.makeachoice.gymratpta.model.contract.user.UserColumns.COLUMN_USER_EMAIL;
-import static me.makeachoice.gymratpta.model.contract.user.UserColumns.COLUMN_USER_NAME;
-import static me.makeachoice.gymratpta.model.contract.user.UserColumns.COLUMN_USER_PHOTO;
-import static me.makeachoice.gymratpta.model.contract.user.UserColumns.COLUMN_USER_STATUS;
-
-/**
- * Created by Usuario on 2/3/2017.
- */
-
 public class ContactsColumns {
 
     // A "projection" defines the columns that will be returned for each row
-    public static final String[] PROJECTION_CONTACTS =
-            {
-                    ContactsContract.Contacts._ID,
-                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
-            };
+    public static final String[] PROJECTION_CONTACTS = {
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+    };
 
     public static final int INDEX_ID = 0;
     public static final int INDEX_PRIMARY_NAME = 1;
+
+    public static final String[] PROJECTION_PHONE = {
+            ContactsContract.CommonDataKinds.Phone.NUMBER
+    };
+
+    public static final int INDEX_PHONE = 0;
+
+    public static final String[] PROJECTION_EMAIL = {
+            ContactsContract.CommonDataKinds.Email.DATA,
+            ContactsContract.CommonDataKinds.Email.TYPE
+    };
+
+    public static final int INDEX_EMAIL_DATA = 0;
+    public static final int INDEX_EMAIL_TYPE = 1;
 
 }
