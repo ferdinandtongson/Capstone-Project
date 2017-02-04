6711b36c951e896fbf26a708984480772338b8d5
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-03 21:57:15 -0800

    Add ClientFBItem to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/ClientFBItem.java
@@ -0,0 +1,16 @@
+package me.makeachoice.gymratpta.model.item.client;
+
+/**
+ * Created by Usuario on 2/3/2017.
+ */
+
+public class ClientFBItem {
+
+    public String clientName;
+    public String email;
+    public String phone;
+    public String firstSession;
+    public String goals;
+    public String status;
+
+}
