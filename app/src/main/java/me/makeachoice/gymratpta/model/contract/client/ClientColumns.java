949f8c610936a1e76abfea7871b7f2e9ec12e402
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-03 21:57:55 -0800

    Update client column values

--- app/src/main/java/me/makeachoice/gymratpta/model/contract/client/ClientColumns.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/client/ClientColumns.java
@@ -4,9 +4,6 @@ package me.makeachoice.gymratpta.model.contract.client;
 /*
  *  UserColumns are columns used in user table in the sqlite database
  */
-
-import android.provider.ContactsContract;
-
 /**************************************************************************************************/
 
 public class ClientColumns {
@@ -26,6 +23,18 @@ public class ClientColumns {
     //client name
     public static final String COLUMN_CLIENT_NAME = "client_name";
 
+    //client email
+    public static final String COLUMN_CLIENT_EMAIL = "client_email";
+
+    //client phone
+    public static final String COLUMN_CLIENT_PHONE = "client_phone";
+
+    //date of first client session
+    public static final String COLUMN_FIRST_SESSION = "first_session";
+
+    //client goals
+    public static final String COLUMN_CLIENT_GOALS = "client_goals";
+
     //client status (active, retired)
     public static final String COLUMN_CLIENT_STATUS = "client_status";
 
@@ -43,15 +52,24 @@ public class ClientColumns {
                     COLUMN_FKEY,
                     COLUMN_CONTACTS_ID,
                     COLUMN_CLIENT_NAME,
+                    COLUMN_CLIENT_EMAIL,
+                    COLUMN_CLIENT_PHONE,
+                    COLUMN_FIRST_SESSION,
+                    COLUMN_CLIENT_GOALS,
                     COLUMN_CLIENT_STATUS,
-                    COLUMN_PROFILE_PIC
+                    COLUMN_PROFILE_PIC,
+
             };
 
     public static final int INDEX_UID = 0;
     public static final int INDEX_FKEY = 1;
     public static final int INDEX_CONTACTS_ID = 2;
     public static final int INDEX_CLIENT_NAME = 3;
-    public static final int INDEX_CLIENT_STATUS = 4;
-    public static final int INDEX_PROFILE_PIC = 5;
+    public static final int INDEX_CLIENT_EMAIL = 4;
+    public static final int INDEX_CLIENT_PHONE = 5;
+    public static final int INDEX_FIRST_SESSION = 6;
+    public static final int INDEX_CLIENT_GOALS = 7;
+    public static final int INDEX_CLIENT_STATUS = 8;
+    public static final int INDEX_PROFILE_PIC = 9;
 
 }
