f682dd18e4555bd0796d69053d61bb60109d8a39
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-05 23:16:54 -0800

    Add projection and index values

--- app/src/main/java/me/makeachoice/gymratpta/model/contract/exercise/CategoryColumns.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/exercise/CategoryColumns.java
@@ -1,24 +1,41 @@
 package me.makeachoice.gymratpta.model.contract.exercise;
 
-/**
- * Created by Usuario on 2/1/2017.
+
+/**************************************************************************************************/
+/*
+ *  CategoryColumns are columns used in category table in the sqlite database
  */
+/**************************************************************************************************/
 
 public class CategoryColumns {
 
     // Table name
     public static final String TABLE_NAME = "category";
 
-    //Firebase key
-    public static final String COLUMN_FIREBASE_KEY = "firebase_key";
+    //user id
+    public static final String COLUMN_UID = "uid";
 
-    //user key
-    public static final String COLUMN_USER_KEY = "user_key";
+    //firebase key
+    public static final String COLUMN_FKEY = "fkey";
 
-    //category name
+    //contacts id
     public static final String COLUMN_CATEGORY_NAME = "category_name";
 
     //default sort order
     public static final String SORT_ORDER_DEFAULT = COLUMN_CATEGORY_NAME + " ASC";
+
+
+    // A "projection" defines the columns that will be returned for each row
+    public static final String[] PROJECTION =
+            {
+                    COLUMN_UID,
+                    COLUMN_FKEY,
+                    COLUMN_CATEGORY_NAME
+            };
+
+    public static final int INDEX_UID = 0;
+    public static final int INDEX_FKEY = 1;
+    public static final int INDEX_CATEGORY_NAME = 2;
+
 
 }
