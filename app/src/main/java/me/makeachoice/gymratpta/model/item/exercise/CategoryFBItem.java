c59fc46687abe61d64c920e9680e748abfccefc5
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-05 23:02:56 -0800

    Add CategoryFBItem to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/exercise/CategoryFBItem.java
@@ -0,0 +1,14 @@
+package me.makeachoice.gymratpta.model.item.exercise;
+
+/**************************************************************************************************/
+/*
+ * CategoryFBItem is used to store exercise category info sent and received from firebase
+ */
+/**************************************************************************************************/
+
+public class CategoryFBItem {
+    public String categoryName;
+
+    public CategoryFBItem(){}
+
+}
