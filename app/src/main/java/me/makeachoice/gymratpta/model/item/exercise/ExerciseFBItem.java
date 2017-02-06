efdb0afbe398585a4def976e0905cfea126b52a3
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-06 13:23:12 -0800

    Add ExerciseFBItem to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/exercise/ExerciseFBItem.java
@@ -0,0 +1,14 @@
+package me.makeachoice.gymratpta.model.item.exercise;
+
+/**
+ * Created by Usuario on 2/6/2017.
+ */
+
+public class ExerciseFBItem {
+
+    public String exerciseName;
+    public String exerciseCategory;
+    public String recordPrimary;
+    public String recordSecondary;
+
+}
