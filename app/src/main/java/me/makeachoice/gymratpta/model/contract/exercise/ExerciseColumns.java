c7030aae3b6fcc164dfe4e07314dbe154592dd7f
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-06 13:41:34 -0800

    Add projections and index

--- app/src/main/java/me/makeachoice/gymratpta/model/contract/exercise/ExerciseColumns.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/exercise/ExerciseColumns.java
@@ -1,5 +1,7 @@
 package me.makeachoice.gymratpta.model.contract.exercise;
 
+import static me.makeachoice.gymratpta.model.contract.exercise.CategoryColumns.COLUMN_CATEGORY_NAME;
+
 /**
  * Created by Usuario on 2/1/2017.
  */
@@ -32,5 +34,26 @@ public class ExerciseColumns {
 
     //default sort order
     public static final String SORT_ORDER_DEFAULT = COLUMN_EXERCISE_NAME + " ASC";
+
+
+    // A "projection" defines the columns that will be returned for each row
+    public static final String[] PROJECTION =
+            {
+                    COLUMN_UID,
+                    COLUMN_CATEGORY_KEY,
+                    COLUMN_FKEY,
+                    COLUMN_EXERCISE_NAME,
+                    COLUMN_EXERCISE_CATEGORY,
+                    COLUMN_RECORD_PRIMARY,
+                    COLUMN_RECORD_SECONDARY
+            };
+
+    public static final int INDEX_UID = 0;
+    public static final int INDEX_CATEGORY_KEY = 1;
+    public static final int INDEX_FKEY = 2;
+    public static final int INDEX_EXERCISE_NAME = 3;
+    public static final int INDEX_EXERCISE_CATEGORY = 4;
+    public static final int INDEX_RECORD_PRIMARY = 5;
+    public static final int INDEX_RECORD_SECONDARY = 6;
 
 }
