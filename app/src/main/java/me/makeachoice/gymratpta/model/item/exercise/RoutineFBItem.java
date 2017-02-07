0c903d0e0270caaed03f2475955c494b2102fdff
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-06 23:58:50 -0800

    Add RoutineFBItem to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/exercise/RoutineFBItem.java
@@ -0,0 +1,11 @@
+package me.makeachoice.gymratpta.model.item.exercise;
+
+import me.makeachoice.gymratpta.model.item.BaseSessionItem;
+
+/**
+ * Created by Usuario on 2/6/2017.
+ */
+
+public class RoutineFBItem extends BaseSessionItem {
+    public String routineName;
+}
