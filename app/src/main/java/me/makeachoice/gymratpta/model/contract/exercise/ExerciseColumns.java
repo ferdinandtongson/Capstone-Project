b851b14058b70f0c2bbbdc31bec77c7b13ed13cb
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-06 22:34:45 -0800

    Remove final from all variables

--- app/src/main/java/me/makeachoice/gymratpta/model/contract/exercise/ExerciseColumns.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/exercise/ExerciseColumns.java
@@ -1,6 +1,5 @@
 package me.makeachoice.gymratpta.model.contract.exercise;
 
-import static me.makeachoice.gymratpta.model.contract.exercise.CategoryColumns.COLUMN_CATEGORY_NAME;
 
 /**
  * Created by Usuario on 2/1/2017.
@@ -9,35 +8,35 @@ import static me.makeachoice.gymratpta.model.contract.exercise.CategoryColumns.C
 public class ExerciseColumns {
 
     // Table name
-    public static final String TABLE_NAME = "exercise";
+    public static String TABLE_NAME = "exercise";
 
     //user id
-    public static final String COLUMN_UID = "uid";
+    public static String COLUMN_UID = "uid";
 
     //category key
-    public static final String COLUMN_CATEGORY_KEY = "category_key";
+    public static String COLUMN_CATEGORY_KEY = "category_key";
 
     //firebase key
-    public static final String COLUMN_FKEY = "fkey";
+    public static String COLUMN_FKEY = "fkey";
 
     //exercise name
-    public static final String COLUMN_EXERCISE_NAME = "exercise_name";
+    public static String COLUMN_EXERCISE_NAME = "exercise_name";
 
     //exercise category
-    public static final String COLUMN_EXERCISE_CATEGORY = "exercise_category";
+    public static String COLUMN_EXERCISE_CATEGORY = "exercise_category";
 
     //primary record data
-    public static final String COLUMN_RECORD_PRIMARY = "record_primary";
+    public static String COLUMN_RECORD_PRIMARY = "record_primary";
 
     //secondary record data
-    public static final String COLUMN_RECORD_SECONDARY = "record_secondary";
+    public static String COLUMN_RECORD_SECONDARY = "record_secondary";
 
     //default sort order
-    public static final String SORT_ORDER_DEFAULT = COLUMN_EXERCISE_NAME + " ASC";
+    public static String SORT_ORDER_DEFAULT = COLUMN_EXERCISE_NAME + " ASC";
 
 
     // A "projection" defines the columns that will be returned for each row
-    public static final String[] PROJECTION =
+    public static String[] PROJECTION_EXERCISE =
             {
                     COLUMN_UID,
                     COLUMN_CATEGORY_KEY,
@@ -48,12 +47,12 @@ public class ExerciseColumns {
                     COLUMN_RECORD_SECONDARY
             };
 
-    public static final int INDEX_UID = 0;
-    public static final int INDEX_CATEGORY_KEY = 1;
-    public static final int INDEX_FKEY = 2;
-    public static final int INDEX_EXERCISE_NAME = 3;
-    public static final int INDEX_EXERCISE_CATEGORY = 4;
-    public static final int INDEX_RECORD_PRIMARY = 5;
-    public static final int INDEX_RECORD_SECONDARY = 6;
+    public static int INDEX_UID = 0;
+    public static int INDEX_CATEGORY_KEY = 1;
+    public static int INDEX_FKEY = 2;
+    public static int INDEX_EXERCISE_NAME = 3;
+    public static int INDEX_EXERCISE_CATEGORY = 4;
+    public static int INDEX_RECORD_PRIMARY = 5;
+    public static int INDEX_RECORD_SECONDARY = 6;
 
 }
