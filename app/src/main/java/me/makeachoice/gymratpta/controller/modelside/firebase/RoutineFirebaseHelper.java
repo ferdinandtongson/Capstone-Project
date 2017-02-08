a05607d9a970ee02f4c5473a280e86d25542a6fd
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-07 17:26:00 -0800

    Use new routine item object

--- app/src/main/java/me/makeachoice/gymratpta/controller/modelside/firebase/RoutineFirebaseHelper.java
+++ app/src/main/java/me/makeachoice/gymratpta/controller/modelside/firebase/RoutineFirebaseHelper.java
@@ -9,6 +9,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 import me.makeachoice.gymratpta.model.item.exercise.RoutineFBItem;
+import me.makeachoice.gymratpta.model.item.exercise.RoutineItem;
 
 
 /**************************************************************************************************/
@@ -25,40 +26,14 @@ public class RoutineFirebaseHelper {
  */
 /**************************************************************************************************/
 
-    public final static String CHILD_ROUTINE_NAME = "routineName";
-    public final static String CHILD_EXERCISE01 = "exercise01";
-    public final static String CHILD_EXERCISE02 = "exercise02";
-    public final static String CHILD_EXERCISE03 = "exercise03";
-    public final static String CHILD_EXERCISE04 = "exercise04";
-    public final static String CHILD_EXERCISE05 = "exercise05";
-    public final static String CHILD_EXERCISE06 = "exercise06";
-    public final static String CHILD_EXERCISE07 = "exercise07";
-    public final static String CHILD_EXERCISE08 = "exercise08";
-    public final static String CHILD_EXERCISE09 = "exercise09";
-    public final static String CHILD_EXERCISE10 = "exercise10";
-    public final static String CHILD_CATEGORY01 = "category01";
-    public final static String CHILD_CATEGORY02 = "category02";
-    public final static String CHILD_CATEGORY03 = "category03";
-    public final static String CHILD_CATEGORY04 = "category04";
-    public final static String CHILD_CATEGORY05 = "category05";
-    public final static String CHILD_CATEGORY06 = "category06";
-    public final static String CHILD_CATEGORY07 = "category07";
-    public final static String CHILD_CATEGORY08 = "category08";
-    public final static String CHILD_CATEGORY09 = "category09";
-    public final static String CHILD_CATEGORY10 = "category10";
-    public final static String CHILD_SET01 = "set01";
-    public final static String CHILD_SET02 = "set02";
-    public final static String CHILD_SET03 = "set03";
-    public final static String CHILD_SET04 = "set04";
-    public final static String CHILD_SET05 = "set05";
-    public final static String CHILD_SET06 = "set06";
-    public final static String CHILD_SET07 = "set07";
-    public final static String CHILD_SET08 = "set08";
-    public final static String CHILD_SET09 = "set09";
-    public final static String CHILD_SET10 = "set10";
+    public static String CHILD_ROUTINE_NAME = "routineName";
+    public static String CHILD_ORDER_NUMBER = "orderNumber";
+    public static String CHILD_EXERCISE = "exercise";
+    public static String CHILD_CATEGORY = "category";
+    public static String CHILD_NUM_SETS = "numOfSets";
 
     //PARENT - parent director
-    private final static String PARENT = "routine";
+    private static String PARENT = "routine";
 
     //mFireDB - firebase instance
     private FirebaseDatabase mFireDB;
@@ -106,8 +81,12 @@ public class RoutineFirebaseHelper {
         return mFireDB.getReference().child(PARENT).child(userId);
     }
 
-    private DatabaseReference getRoutineDetailReference(String userId, String fkey){
-        return getRoutineReference(userId).child(fkey);
+    private DatabaseReference getRoutineDetailReference(String userId, String routineName){
+        return getRoutineReference(userId).child(routineName);
+    }
+
+    private DatabaseReference getRoutineExerciseReference(String userId, String routineName, String orderNumber){
+        return getRoutineReference(userId).child(routineName).child(orderNumber);
     }
 
 /**************************************************************************************************/
@@ -118,17 +97,26 @@ public class RoutineFirebaseHelper {
  */
 /**************************************************************************************************/
 
-    public void addRoutineData(String userId, ArrayList<RoutineFBItem> routines){
+    public void addRoutineData(String userId, ArrayList<RoutineItem> routines){
+        RoutineItem routine;
+
         int count = routines.size();
         for(int i = 0; i < count; i++){
-            addRoutine(userId, routines.get(i));
+            routine = routines.get(i);
+
+            RoutineFBItem item = new RoutineFBItem();
+            item.exercise = routine.exercise;
+            item.category = routine.category;
+            item.numOfSets = routine.numOfSets;
+            addRoutine(userId, routine, item);
         }
 
     }
 
-    public void addRoutine(String userId, RoutineFBItem item){
-        DatabaseReference ref = getRoutineReference(userId);
-        ref.push().setValue(item);
+    public void addRoutine(String userId, RoutineItem routine, RoutineFBItem item){
+        String strIndex = String.valueOf(routine.orderNumber);
+        DatabaseReference ref = getRoutineExerciseReference(userId, routine.routineName, strIndex);
+        ref.setValue(item);
     }
 
 /**************************************************************************************************/
@@ -139,68 +127,17 @@ public class RoutineFirebaseHelper {
  */
 /**************************************************************************************************/
 
-    public void setRoutineName(String userId, String fkey, String name){
-        DatabaseReference refRoutine = getRoutineDetailReference(userId, fkey);
+    public void setRoutineName(String userId, String routineName, String name){
+        DatabaseReference refRoutine = getRoutineDetailReference(userId, routineName);
 
         refRoutine.child(CHILD_ROUTINE_NAME).setValue(name);
     }
 
-    public void setRoutineDetail(String userId, String fkey, int index, String exercise, String category, int set){
-        DatabaseReference refRoutine = getRoutineDetailReference(userId, fkey);
+    public void setRoutineDetail(String userId, String routineName, int index, String exercise, String category, int set){
+        DatabaseReference refRoutine = getRoutineDetailReference(userId, routineName);
 
-        switch(index){
-            case 1:
-                refRoutine.child(CHILD_EXERCISE01).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY01).setValue(category);
-                refRoutine.child(CHILD_SET01).setValue(set);
-                break;
-            case 2:
-                refRoutine.child(CHILD_EXERCISE02).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY02).setValue(category);
-                refRoutine.child(CHILD_SET02).setValue(set);
-                break;
-            case 3:
-                refRoutine.child(CHILD_EXERCISE03).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY03).setValue(category);
-                refRoutine.child(CHILD_SET03).setValue(set);
-                break;
-            case 4:
-                refRoutine.child(CHILD_EXERCISE04).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY04).setValue(category);
-                refRoutine.child(CHILD_SET04).setValue(set);
-                break;
-            case 5:
-                refRoutine.child(CHILD_EXERCISE05).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY05).setValue(category);
-                refRoutine.child(CHILD_SET05).setValue(set);
-                break;
-            case 6:
-                refRoutine.child(CHILD_EXERCISE06).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY06).setValue(category);
-                refRoutine.child(CHILD_SET06).setValue(set);
-                break;
-            case 7:
-                refRoutine.child(CHILD_EXERCISE07).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY07).setValue(category);
-                refRoutine.child(CHILD_SET07).setValue(set);
-                break;
-            case 8:
-                refRoutine.child(CHILD_EXERCISE08).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY08).setValue(category);
-                refRoutine.child(CHILD_SET08).setValue(set);
-                break;
-            case 9:
-                refRoutine.child(CHILD_EXERCISE09).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY09).setValue(category);
-                refRoutine.child(CHILD_SET09).setValue(set);
-                break;
-            case 10:
-                refRoutine.child(CHILD_EXERCISE10).setValue(exercise);
-                refRoutine.child(CHILD_CATEGORY10).setValue(category);
-                refRoutine.child(CHILD_SET10).setValue(set);
-                break;
-        }
-    }
+
+  }
 
 /**************************************************************************************************/
 
@@ -230,13 +167,13 @@ public class RoutineFirebaseHelper {
         ref.orderByChild(orderBy).addListenerForSingleValueEvent(mEventListener);
     }
 
-    public void requestRoutineDataByName(String userId, String name, OnDataLoadedListener listener){
+    public void requestRoutineDetailByName(String userId, String routineName, OnDataLoadedListener listener){
         //get reference
-        DatabaseReference ref = getRoutineReference(userId);
+        DatabaseReference ref = getRoutineDetailReference(userId, routineName);
 
         mOnDataLoadedListener = listener;
 
-        ref.orderByChild(CHILD_ROUTINE_NAME).equalTo(name).addListenerForSingleValueEvent(mEventListener);
+        ref.orderByChild(CHILD_ORDER_NUMBER).addListenerForSingleValueEvent(mEventListener);
 
     }
 
