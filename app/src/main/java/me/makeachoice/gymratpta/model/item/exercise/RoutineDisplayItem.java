e8d08d0c0b0836bdccf275dc0ffc6a7b8020d0ed
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-08 09:29:15 -0800

    Add RoutineDisplayItem to project, used for Routine screen adapter

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/exercise/RoutineDisplayItem.java
@@ -0,0 +1,12 @@
+package me.makeachoice.gymratpta.model.item.exercise;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Usuario on 2/7/2017.
+ */
+
+public class RoutineDisplayItem {
+    public String routineName;
+    public ArrayList<RoutineItem> routineExercises;
+}
