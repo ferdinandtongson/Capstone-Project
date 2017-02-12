e5a82346ce9d9634bbc71d74eef8c53fd38efcf0
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-12 08:57:44 -0800

    Cleanup code and add comments

--- app/src/main/java/me/makeachoice/gymratpta/controller/viewside/bottomnav/SaveNav.java
+++ app/src/main/java/me/makeachoice/gymratpta/controller/viewside/bottomnav/SaveNav.java
@@ -4,27 +4,38 @@ import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
 
 import me.makeachoice.gymratpta.R;
-import me.makeachoice.gymratpta.controller.manager.MaidRegistry;
-import me.makeachoice.gymratpta.controller.viewside.maid.MyMaid;
 import me.makeachoice.library.android.base.view.activity.MyActivity;
 
 import static me.makeachoice.gymratpta.R.id.bottom_nav_item1;
 import static me.makeachoice.gymratpta.R.id.bottom_nav_item2;
 
 
-/**
- * Created by Usuario on 2/8/2017.
+/**************************************************************************************************/
+/*
+ * SaveNav gives the user the option to save or cancel a exercise training routine using the bottom
+ * navigation component
  */
+/**************************************************************************************************/
 
 public class SaveNav extends MyBottomNav implements BottomNavigationView.OnNavigationItemSelectedListener{
 
 /**************************************************************************************************/
 /*
  * Class Variables:
+ *      DEFAULT_MENU_ID - default menu id
+ *      mListener - listens for an onSave click event
  */
 /**************************************************************************************************/
 
+    //DEFAULT_MENU_ID - default menu id
     private final static int DEFAULT_MENU_ID = R.menu.bottom_nav_save_menu;
+
+    //mListener - listens for an onSave click event
+    private OnSaveRoutineListener mListener;
+    public interface OnSaveRoutineListener{
+        //notify listener of an onSave click event
+        public void onSaveRoutine();
+    }
 
 /**************************************************************************************************/
 
@@ -56,11 +67,25 @@ public class SaveNav extends MyBottomNav implements BottomNavigationView.OnNavig
         initializeNavigation(menuId);
     }
 
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Initialization Methods
+ *      void initializeNavigation(int) - initialize bottom navigation menu components
+ */
+/**************************************************************************************************/
+    /*
+     * void initializeNavigation(int) - initialize bottom navigation menu components
+     */
     private void initializeNavigation(int menuId){
         //inflate menu
         mNav.inflateMenu(menuId);
 
+        //set onNavigationItemSelected listener
         mNav.setOnNavigationItemSelectedListener(this);
+
+        //deselect menu items
         mNav.getMenu().getItem(0).setChecked(false);
         mNav.getMenu().getItem(1).setChecked(false);
     }
@@ -69,30 +94,48 @@ public class SaveNav extends MyBottomNav implements BottomNavigationView.OnNavig
 
 /**************************************************************************************************/
 /*
- * Select Item Methods
- *      void setOnItemSelectedListener(OnNavigationItemSelectedListener) - listener for item select events
- *      void checkItem(int) - mark menu item as checked
- *      void uncheckAll() - mark all menu items as unchecked
+ * Setter Methods
+ *      void setSaveListener(...) - set listener to listen for onSave click event
  */
 /**************************************************************************************************/
+    /*
+     * void setSaveListener(...) - set listener to listen for onSave click event
+     */
+    public void setSaveListener(OnSaveRoutineListener listener){
+        mListener = listener;
+    }
 
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Event Methods
+ *      boolean onNavigationItemSelected(MenuItem) - bottom navigation menu item selected
+ */
+/**************************************************************************************************/
+    /*
+     * boolean onNavigationItemSelected(MenuItem) - bottom navigation menu item selected
+     */
     public boolean onNavigationItemSelected(MenuItem item){
+        //get menu id
         int itemId = item.getItemId();
-        uncheckAll();
-        item.setChecked(true);
 
-        MyMaid maid;
-        MaidRegistry maidRegistry = MaidRegistry.getInstance();
-
+        //find which menu item was selected
         switch (itemId) {
-            case bottom_nav_item1: // 0 - save
-                //TODO - save routine
-                break;
-            case bottom_nav_item2: // 1 - cancel
+            case bottom_nav_item1: // 0 - cancel
+                //onCancel event, return to parent activity
                 mActivity.onBackPressed();
                 break;
-            default:
-                maid = null;
+            case bottom_nav_item2: // 1 - save
+                //onSave event, check if there is a listener
+                if(mListener != null){
+                    //notify listener of onSave click event
+                    mListener.onSaveRoutine();
+                }
+
+                //return to parent activity
+                mActivity.onBackPressed();
+                break;
         }
 
         return false;
