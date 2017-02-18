76bf4cda9e5af2c44f42aec042ad9a65c43097bc
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-17 19:36:16 -0800

    Add appointment firebase helper to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/controller/modelside/firebase/client/AppointmentFirebaseHelper.java
@@ -0,0 +1,165 @@
+package me.makeachoice.gymratpta.controller.modelside.firebase.client;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import me.makeachoice.gymratpta.model.item.client.AppointmentFBItem;
+
+/**************************************************************************************************/
+/*
+ * AppointmentFirebaseHelper helps in adding and requesting client appointment data to and from firebase
+ */
+/**************************************************************************************************/
+
+public class AppointmentFirebaseHelper {
+
+/**************************************************************************************************/
+/*
+ * Class Variables:
+ */
+/**************************************************************************************************/
+
+    public final static String CHILD_APPOINTMENT_TIME = "appointmentTime";
+    public final static String CHILD_CLIENT_KEY = "clientKey";
+    public final static String CHILD_CLIENT_NAME = "clientName";
+    public final static String CHILD_STATUS = "status";
+
+    //PARENT - parent director
+    private final static String PARENT = "appointment";
+
+    //mFireDB - firebase instance
+    private FirebaseDatabase mFireDB;
+
+    private OnDataLoadedListener mOnDataLoadedListener;
+
+    //Implemented communication bridge
+    public interface OnDataLoadedListener{
+        //called when requested data has been loaded
+        void onDataLoaded(DataSnapshot dataSnapshot);
+        void onCancelled();
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Constructor:
+ */
+/**************************************************************************************************/
+
+    private static AppointmentFirebaseHelper instance = null;
+
+    public static AppointmentFirebaseHelper getInstance() {
+        if(instance == null) {
+            instance = new AppointmentFirebaseHelper();
+        }
+        return instance;
+    }
+
+    private AppointmentFirebaseHelper(){
+
+        mFireDB = FirebaseDatabase.getInstance();
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Getter Methods
+ */
+/**************************************************************************************************/
+
+    public DatabaseReference getAppointmentReference(String userId){
+        return mFireDB.getReference().child(PARENT).child(userId);
+    }
+
+    private DatabaseReference getAppointmentReferenceByDay(String userId, String appointmentDay){
+        return getAppointmentReference(userId).child(appointmentDay);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Add Data Methods
+ */
+/**************************************************************************************************/
+
+    public void addAppointmentDataByDay(String userId, String appointmentDay, ArrayList<AppointmentFBItem> appointments){
+        int count = appointments.size();
+        for(int i = 0; i < count; i++){
+            addAppointmentByDay(userId, appointmentDay, appointments.get(i));
+        }
+
+    }
+
+    public void addAppointmentByDay(String userId, String appointmentDay, AppointmentFBItem item){
+        DatabaseReference ref = getAppointmentReferenceByDay(userId, appointmentDay);
+        ref.push().setValue(item);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Set Data Methods
+ */
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Request Data Methods
+ */
+/**************************************************************************************************/
+
+    public void requestAppointmentDataByDate(String userId, String appointmentDate, OnDataLoadedListener listener){
+        //get reference
+        DatabaseReference ref = getAppointmentReferenceByDay(userId, appointmentDate);
+
+        mOnDataLoadedListener = listener;
+
+        //add event listener to reference
+        ref.addListenerForSingleValueEvent(mEventListener);
+    }
+
+    public void requestAppointmentDataByDate(String userId, String appointmentDate, String orderBy,
+                                                OnDataLoadedListener listener){
+        //get reference
+        DatabaseReference ref = getAppointmentReferenceByDay(userId, appointmentDate);
+
+        mOnDataLoadedListener = listener;
+
+        //add event listener to reference
+        ref.orderByChild(orderBy).addListenerForSingleValueEvent(mEventListener);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * EventListeners
+ */
+/**************************************************************************************************/
+
+    private ValueEventListener mEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            mOnDataLoadedListener.onDataLoaded(dataSnapshot);
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            mOnDataLoadedListener.onCancelled();
+        }
+    };
+
+/**************************************************************************************************/
+
+}
