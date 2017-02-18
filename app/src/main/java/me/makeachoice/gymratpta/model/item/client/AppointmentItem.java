96c56d836296d870afade4a6b240f7c82dd9e39d
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-17 19:40:30 -0800

    Add appointment item object used to local database

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/AppointmentItem.java
@@ -0,0 +1,82 @@
+package me.makeachoice.gymratpta.model.item.client;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import me.makeachoice.gymratpta.model.contract.Contractor;
+import me.makeachoice.gymratpta.model.contract.client.AppointmentColumns;
+
+/**************************************************************************************************/
+/*
+ * AppointmentItem, extends AppointmentFBItem, used to interface with local database
+ */
+/**************************************************************************************************/
+
+public class AppointmentItem extends AppointmentFBItem {
+
+/**************************************************************************************************/
+/*
+ * Class Variables
+ */
+/**************************************************************************************************/
+
+    public String uid;
+    public String fkey;
+    public String appointmentDay;
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Constructor
+ */
+/**************************************************************************************************/
+
+    public AppointmentItem(){}
+
+    public AppointmentItem(AppointmentFBItem item){
+        appointmentTime = item.appointmentTime;
+        clientKey = item.clientKey;
+        clientName = item.clientName;
+        status = item.status;
+    }
+
+    public AppointmentItem(Cursor cursor){
+        uid = cursor.getString(AppointmentColumns.INDEX_UID);
+        fkey = cursor.getString(AppointmentColumns.INDEX_FKEY);
+        appointmentDay = cursor.getString(AppointmentColumns.INDEX_APPOINTMENT_DATE);
+        appointmentTime = cursor.getString(AppointmentColumns.INDEX_APPOINTMENT_TIME);
+        clientKey = cursor.getString(AppointmentColumns.INDEX_CLIENT_KEY);
+        clientName = cursor.getString(AppointmentColumns.INDEX_CLIENT_NAME);
+        status = cursor.getString(AppointmentColumns.INDEX_APPOINTMENT_STATUS);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Public Methods
+ *      ContentValues getContentValues() - convert item data to a ContentValues object
+ *      ClientItem addData(Cursor) - add cursor data to item
+ */
+/**************************************************************************************************/
+    /*
+     * ContentValues getContentValues() - convert item data to a ContentValues object
+     */
+    public ContentValues getContentValues(){
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(Contractor.AppointmentEntry.COLUMN_UID, uid);
+        values.put(Contractor.AppointmentEntry.COLUMN_FKEY, fkey);
+        values.put(Contractor.AppointmentEntry.COLUMN_APPOINTMENT_DATE, appointmentDay);
+        values.put(Contractor.AppointmentEntry.COLUMN_APPOINTMENT_TIME, appointmentTime);
+        values.put(Contractor.AppointmentEntry.COLUMN_CLIENT_KEY, clientKey);
+        values.put(Contractor.AppointmentEntry.COLUMN_CLIENT_NAME, clientName);
+        values.put(Contractor.AppointmentEntry.COLUMN_APPOINTMENT_STATUS, status);
+
+        return values;
+    }
+
+/**************************************************************************************************/
+
+}
