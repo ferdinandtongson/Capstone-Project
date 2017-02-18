d941e2b1961045f5f6f8deb4741118d5cfbb4049
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-17 19:38:25 -0800

    Add client appointment firebase item object to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/ClientAppFBItem.java
@@ -0,0 +1,14 @@
+package me.makeachoice.gymratpta.model.item.client;
+
+/**
+ * Created by Usuario on 2/3/2017.
+ */
+
+public class ClientAppFBItem {
+
+    public String appointmentDate;
+    public String appointmentTime;
+    public String clientName;
+    public String status;
+
+}
