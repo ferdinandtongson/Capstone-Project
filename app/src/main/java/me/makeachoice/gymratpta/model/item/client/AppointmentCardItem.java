1cc671edf9c33d17afd13b4f346435fcac49ea3b
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-19 11:57:18 -0800

    Add AppointmentCardItem to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/AppointmentCardItem.java
@@ -0,0 +1,19 @@
+package me.makeachoice.gymratpta.model.item.client;
+
+import android.net.Uri;
+
+/**
+ * Created by Usuario on 12/24/2016.
+ */
+
+public class AppointmentCardItem {
+
+    public String clientName;
+    public String clientInfo;
+    public Uri profilePic;
+    public String routineName;
+    public boolean isActive;
+
+    public AppointmentCardItem(){}
+
+}
