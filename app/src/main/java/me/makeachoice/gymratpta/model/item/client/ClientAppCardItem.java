0c81f816b382f12ed94b89a5af049cd798406e0c
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-19 15:50:20 -0800

    Add client appointment card item object to project

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/ClientAppCardItem.java
@@ -0,0 +1,18 @@
+package me.makeachoice.gymratpta.model.item.client;
+
+import android.net.Uri;
+
+/**
+ * Created by Usuario on 12/24/2016.
+ */
+
+public class ClientAppCardItem {
+
+    public String appointmentDate;
+    public String appointmentTime;
+    public String routineName;
+    public boolean isActive;
+
+    public ClientAppCardItem(){}
+
+}
