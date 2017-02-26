f3afe6bcd19f91bd7690d0e6568ff686b54975a4
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-26 06:31:14 -0800

    Refactor ScheduleItem from AppointmentItem

--- app/src/main/java/me/makeachoice/gymratpta/model/item/client/ScheduleItem.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/ScheduleItem.java
@@ -3,16 +3,15 @@ package me.makeachoice.gymratpta.model.item.client;
 import android.content.ContentValues;
 import android.database.Cursor;
 
-import me.makeachoice.gymratpta.model.contract.Contractor;
-import me.makeachoice.gymratpta.model.contract.client.AppointmentColumns;
+import me.makeachoice.gymratpta.model.contract.client.ScheduleContract;
 
 /**************************************************************************************************/
 /*
- * AppointmentItem, extends AppointmentFBItem, used to interface with local database
+ * ScheduleItem appointment schedule item used for local database
  */
 /**************************************************************************************************/
 
-public class AppointmentItem extends AppointmentFBItem {
+public class ScheduleItem extends ScheduleFBItem {
 
 /**************************************************************************************************/
 /*
@@ -21,8 +20,7 @@ public class AppointmentItem extends AppointmentFBItem {
 /**************************************************************************************************/
 
     public String uid;
-    public String fkey;
-    public String appointmentDate;
+    public String datestamp;
 
 /**************************************************************************************************/
 
@@ -32,9 +30,10 @@ public class AppointmentItem extends AppointmentFBItem {
  */
 /**************************************************************************************************/
 
-    public AppointmentItem(){}
+    public ScheduleItem(){}
 
-    public AppointmentItem(AppointmentFBItem item){
+    public ScheduleItem(ScheduleFBItem item){
+        appointmentDate = item.appointmentDate;
         appointmentTime = item.appointmentTime;
         clientKey = item.clientKey;
         clientName = item.clientName;
@@ -42,7 +41,7 @@ public class AppointmentItem extends AppointmentFBItem {
         status = item.status;
     }
 
-    public AppointmentItem(ClientAppFBItem item){
+    public ScheduleItem(ClientAppFBItem item){
         appointmentDate = item.appointmentDate;
         appointmentTime = item.appointmentTime;
         clientName = item.clientName;
@@ -50,15 +49,15 @@ public class AppointmentItem extends AppointmentFBItem {
         status = item.status;
     }
 
-    public AppointmentItem(Cursor cursor){
-        uid = cursor.getString(AppointmentColumns.INDEX_UID);
-        fkey = cursor.getString(AppointmentColumns.INDEX_FKEY);
-        appointmentDate = cursor.getString(AppointmentColumns.INDEX_APPOINTMENT_DATE);
-        appointmentTime = cursor.getString(AppointmentColumns.INDEX_APPOINTMENT_TIME);
-        clientKey = cursor.getString(AppointmentColumns.INDEX_CLIENT_KEY);
-        clientName = cursor.getString(AppointmentColumns.INDEX_CLIENT_NAME);
-        routineName = cursor.getString(AppointmentColumns.INDEX_ROUTINE_NAME);
-        status = cursor.getString(AppointmentColumns.INDEX_APPOINTMENT_STATUS);
+    public ScheduleItem(Cursor cursor){
+        uid = cursor.getString(ScheduleContract.INDEX_UID);
+        datestamp = cursor.getString(ScheduleContract.INDEX_TIMESTAMP);
+        appointmentDate = cursor.getString(ScheduleContract.INDEX_APPOINTMENT_DATE);
+        appointmentTime = cursor.getString(ScheduleContract.INDEX_APPOINTMENT_TIME);
+        clientKey = cursor.getString(ScheduleContract.INDEX_CLIENT_KEY);
+        clientName = cursor.getString(ScheduleContract.INDEX_CLIENT_NAME);
+        routineName = cursor.getString(ScheduleContract.INDEX_ROUTINE_NAME);
+        status = cursor.getString(ScheduleContract.INDEX_APPOINTMENT_STATUS);
     }
 
 /**************************************************************************************************/
@@ -67,23 +66,22 @@ public class AppointmentItem extends AppointmentFBItem {
 /*
  * Public Methods
  *      ContentValues getContentValues() - convert item data to a ContentValues object
- *      ClientItem addData(Cursor) - add cursor data to item
  */
 /**************************************************************************************************/
     /*
      * ContentValues getContentValues() - convert item data to a ContentValues object
      */
     public ContentValues getContentValues(){
-        // Add a new student record
+        //create content value
         ContentValues values = new ContentValues();
-        values.put(Contractor.AppointmentEntry.COLUMN_UID, uid);
-        values.put(Contractor.AppointmentEntry.COLUMN_FKEY, fkey);
-        values.put(Contractor.AppointmentEntry.COLUMN_APPOINTMENT_DATE, appointmentDate);
-        values.put(Contractor.AppointmentEntry.COLUMN_APPOINTMENT_TIME, appointmentTime);
-        values.put(Contractor.AppointmentEntry.COLUMN_CLIENT_KEY, clientKey);
-        values.put(Contractor.AppointmentEntry.COLUMN_CLIENT_NAME, clientName);
-        values.put(Contractor.AppointmentEntry.COLUMN_ROUTINE_NAME, routineName);
-        values.put(Contractor.AppointmentEntry.COLUMN_APPOINTMENT_STATUS, status);
+        values.put(ScheduleContract.COLUMN_UID, uid);
+        values.put(ScheduleContract.COLUMN_TIMESTAMP, datestamp);
+        values.put(ScheduleContract.COLUMN_APPOINTMENT_DATE, appointmentDate);
+        values.put(ScheduleContract.COLUMN_APPOINTMENT_TIME, appointmentTime);
+        values.put(ScheduleContract.COLUMN_CLIENT_KEY, clientKey);
+        values.put(ScheduleContract.COLUMN_CLIENT_NAME, clientName);
+        values.put(ScheduleContract.COLUMN_ROUTINE_NAME, routineName);
+        values.put(ScheduleContract.COLUMN_APPOINTMENT_STATUS, status);
 
         return values;
     }
