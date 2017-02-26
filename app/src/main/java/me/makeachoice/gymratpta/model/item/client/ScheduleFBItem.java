71f93776b74cec6e669c85229b5ebc2996ec228c
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-26 06:28:23 -0800

    Refactor to ScheduleFBItem from AppointmentFBItem

---
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/ScheduleFBItem.java
@@ -0,0 +1,18 @@
+package me.makeachoice.gymratpta.model.item.client;
+
+/**************************************************************************************************/
+/*
+ * ScheduleFBItem appointment schedule item for firebase
+ */
+/**************************************************************************************************/
+
+public class ScheduleFBItem {
+
+    public String appointmentDate;
+    public String appointmentTime;
+    public String clientKey;
+    public String clientName;
+    public String routineName;
+    public String status;
+
+}
