a1bd63418afbe82f337cff6d830344ba939ce5e9
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-26 11:50:17 -0800

    First commit

---
+++ app/src/main/java/me/makeachoice/gymratpta/controller/modelside/butler/ClientButler.java
@@ -0,0 +1,135 @@
+package me.makeachoice.gymratpta.controller.modelside.butler;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import me.makeachoice.gymratpta.controller.modelside.loader.ClientLoader;
+import me.makeachoice.gymratpta.model.item.client.AppointmentCardItem;
+import me.makeachoice.gymratpta.model.item.client.ScheduleItem;
+import me.makeachoice.gymratpta.model.item.client.ClientItem;
+import me.makeachoice.library.android.base.view.activity.MyActivity;
+
+/**************************************************************************************************/
+/*
+ *  ClientButler assists in loading, saving and deleting client data to database and firebase
+ */
+/**************************************************************************************************/
+
+public class ClientButler {
+
+/**************************************************************************************************/
+/*
+ *  Class Variables
+ */
+/**************************************************************************************************/
+
+    //mActivity - activity context
+    private MyActivity mActivity;
+
+    //mUserId - user authentication id
+    private String mUserId;
+
+    //mClientKey - client key
+    private String mClientKey;
+
+    //mAppointmentItem - appointment item
+    private ScheduleItem mAppointmentItem;
+
+    //mLoaderId - loader id number
+    private int mLoaderId;
+
+    //mClientLoader - loader class used to load client data
+    private ClientLoader mClientLoader;
+
+    //mLoadListener - used to listen for onScheduleLoaded events
+    private OnClientLoadedListener mLoadListener;
+    public interface OnClientLoadedListener{
+        public void onClientLoaded(ClientItem clientItem, AppointmentCardItem cardItem);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Constructor
+ */
+/**************************************************************************************************/
+
+    public ClientButler(MyActivity activity, String userId){
+        mActivity = activity;
+        mUserId = userId;
+
+        //mScheduleList = new ArrayList<>();
+        mClientLoader = new ClientLoader(mActivity, mUserId);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Load Client
+ */
+/**************************************************************************************************/
+    /*
+     * void loadClient() - load client data from database
+     */
+    public void loadClient(int loaderId, ScheduleItem item, OnClientLoadedListener listener){
+        mLoaderId = loaderId;
+        mAppointmentItem = item;
+        mClientKey = item.clientKey;
+        mLoadListener = listener;
+
+        //load client data using client firebase key
+        mClientLoader.loadClientsByFKey(mClientKey, mLoaderId, new ClientLoader.OnClientLoadListener() {
+            @Override
+            public void onClientLoadFinished(Cursor cursor) {
+                onClientLoaded(cursor);
+            }
+        });
+    }
+
+    /*
+     * void onClientLoaded(Cursor) - client data from database has been loaded
+     */
+    private void onClientLoaded(Cursor cursor){
+        AppointmentCardItem cardItem = null;
+        ClientItem clientItem = null;
+
+        //check that cursor is Not null or empty
+        if(cursor != null && cursor.getCount() > 0){
+            //move cursor to first index position
+            cursor.moveToFirst();
+
+            //create client item from cursor data
+            clientItem = new ClientItem(cursor);
+
+            cardItem = createAppointmentCardItem(mAppointmentItem, clientItem);
+        }
+        //destroy loader
+        mClientLoader.destroyLoader(mLoaderId);
+
+        if(mLoadListener != null){
+            mLoadListener.onClientLoaded(clientItem, cardItem);
+        }
+
+    }
+
+    /*
+     * createAppointmentCardItem(...) - create appointmentCard item consumed by adapter
+     */
+    private AppointmentCardItem createAppointmentCardItem(ScheduleItem appItem, ClientItem clientItem){
+        //create appointmentCard item used by adapter
+        AppointmentCardItem item = new AppointmentCardItem();
+        item.clientName = clientItem.clientName;
+        item.clientInfo = appItem.appointmentTime;
+        item.profilePic = Uri.parse(clientItem.profilePic);
+        item.routineName = appItem.routineName;
+        item.isActive = true;
+
+        return item;
+    }
+
+/**************************************************************************************************/
+
+}
---
+++ app/src/main/java/me/makeachoice/gymratpta/controller/modelside/butler/ScheduleDayButler.java
@@ -0,0 +1,324 @@
+package me.makeachoice.gymratpta.controller.modelside.butler;
+
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import me.makeachoice.gymratpta.controller.modelside.firebase.client.ClientScheduleFirebaseHelper;
+import me.makeachoice.gymratpta.controller.modelside.firebase.client.ScheduleFirebaseHelper;
+import me.makeachoice.gymratpta.controller.modelside.loader.ScheduleLoader;
+import me.makeachoice.gymratpta.controller.modelside.query.ScheduleQueryHelper;
+import me.makeachoice.gymratpta.model.contract.client.ScheduleContract;
+import me.makeachoice.gymratpta.model.item.client.ScheduleFBItem;
+import me.makeachoice.gymratpta.model.item.client.ScheduleItem;
+import me.makeachoice.gymratpta.model.item.client.ClientAppFBItem;
+import me.makeachoice.library.android.base.view.activity.MyActivity;
+
+/**************************************************************************************************/
+/*
+ *  ScheduleDayButler assists in loading, saving and deleting appointment schedules to database and firebase
+ */
+/**************************************************************************************************/
+
+public class ScheduleDayButler {
+
+/**************************************************************************************************/
+/*
+ *  Class Variables
+ */
+/**************************************************************************************************/
+
+    //mActivity - activity context
+    private MyActivity mActivity;
+
+    //mUserId - user authentication id
+    private String mUserId;
+
+    //mSaveItem - appointment item to save to database
+    private ScheduleItem mSaveItem;
+
+    //mDeleteItem - appointment item to delete from database
+    private ScheduleItem mDeleteItem;
+
+    //mScheduleList - list of scheduled appointments loaded from database
+    private ArrayList<ScheduleItem> mScheduleList;
+
+    //mLoaderId - loader id number
+    private int mLoaderId;
+
+    //mScheduleLoader - loader class used to load schedule data
+    private ScheduleLoader mScheduleLoader;
+
+    //mLoadListener - used to listen for onScheduleLoaded events
+    private OnScheduleLoadedListener mLoadListener;
+    public interface OnScheduleLoadedListener{
+        public void onScheduleLoaded(ArrayList<ScheduleItem> scheduleList);
+    }
+
+    //mSaveListener - used to listen for onScheduleSaved events
+    private OnScheduleSavedListener mSaveListener;
+    public interface OnScheduleSavedListener{
+        public void onScheduleSaved();
+    }
+
+    //mDeleteListener - used to listen for onScheduleDeleted events
+    private OnScheduleDeletedListener mDeleteListener;
+    public interface OnScheduleDeletedListener{
+        public void onScheduleDeleted();
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Constructor
+ */
+/**************************************************************************************************/
+
+    public ScheduleDayButler(MyActivity activity, String userId){
+        mActivity = activity;
+        mUserId = userId;
+
+        mScheduleList = new ArrayList<>();
+        mScheduleLoader = new ScheduleLoader(mActivity, mUserId);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Load Schedule
+ */
+/**************************************************************************************************/
+    /*
+     * void loadSchedule() - load schedule data for given day from database
+     */
+    public void loadSchedule(int loaderId, String datestamp, OnScheduleLoadedListener listener){
+        mLoaderId = loaderId;
+        mLoadListener = listener;
+
+        //start loader to get schedule data from database
+        mScheduleLoader.loadScheduleByTimestamp(datestamp, mLoaderId,
+                new ScheduleLoader.OnScheduleLoadListener() {
+                    @Override
+                    public void onScheduleLoadFinished(Cursor cursor){
+                        onScheduleLoaded(cursor);
+                    }
+                });
+    }
+
+    /*
+     * void onScheduleLoaded(Cursor) - schedule from database has been loaded
+     */
+    private void onScheduleLoaded(Cursor cursor){
+        //clear schedule list array
+        mScheduleList.clear();
+
+        //get number of scheduled appointments loaded
+        int count = cursor.getCount();
+
+        //loop through cursor
+        for(int i = 0; i < count; i++){
+            //move cursor to index position
+            cursor.moveToPosition(i);
+
+            //create item from cursor data
+            ScheduleItem item = new ScheduleItem(cursor);
+
+            //add item to list
+            mScheduleList.add(item);
+        }
+
+        if(mLoadListener != null){
+            mLoadListener.onScheduleLoaded(mScheduleList);
+        }
+
+        //destroy loader
+        mScheduleLoader.destroyLoader(mLoaderId);
+
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Save Schedule
+ */
+/**************************************************************************************************/
+
+    public void saveSchedule(ScheduleItem saveItem, OnScheduleSavedListener listener){
+        //save listener
+        mSaveListener = listener;
+
+        //save to firebase
+        saveScheduleToFirebase(saveItem);
+
+        //save to local database
+        saveScheduleToDatabase(saveItem);
+
+    }
+
+    /*
+     * void saveScheduleToFirebase(ScheduleItem) - save to firebase
+     */
+    private void saveScheduleToFirebase(ScheduleItem saveItem){
+        //save schedule to schedule firebase
+        saveScheduleToScheduleFB(saveItem);
+
+        //save schedule to client schedule firebase
+        saveScheduleToClientScheduleFB(saveItem);
+    }
+
+    /*
+     * void saveScheduleToScheduleFB(ScheduleItem) - save schedule to schedule firebase
+     */
+    private void saveScheduleToScheduleFB(ScheduleItem saveItem){
+        //get schedule firebase helper instance
+        ScheduleFirebaseHelper appointmentFB = ScheduleFirebaseHelper.getInstance();
+
+        //create schedule firebase item
+        ScheduleFBItem appointmentFBItem = new ScheduleFBItem();
+        appointmentFBItem.appointmentDate = saveItem.appointmentDate;
+        appointmentFBItem.appointmentTime = saveItem.appointmentTime;
+        appointmentFBItem.clientKey = saveItem.clientKey;
+        appointmentFBItem.clientName = saveItem.clientName;
+        appointmentFBItem.routineName = saveItem.routineName;
+        appointmentFBItem.status = saveItem.status;
+
+        //save schedule item to firebase
+        appointmentFB.addScheduleByTimestamp(mUserId, saveItem.datestamp, appointmentFBItem);
+
+    }
+
+    /*
+     * void saveScheduleToClientScheduleFB(ScheduleItem) - save schedule to client schedule firebase
+     */
+    private void saveScheduleToClientScheduleFB(ScheduleItem saveItem){
+        //get client schedule firebase helper instance
+        ClientScheduleFirebaseHelper clientFB = ClientScheduleFirebaseHelper.getInstance();
+
+        //create client schedule firebase item
+        ClientAppFBItem clientFBItem = new ClientAppFBItem();
+        clientFBItem.appointmentDate = saveItem.appointmentDate;
+        clientFBItem.appointmentTime = saveItem.appointmentTime;
+        clientFBItem.clientName = saveItem.clientName;
+        clientFBItem.routineName = saveItem.routineName;
+        clientFBItem.status = saveItem.status;
+
+        //save client schedule to firebase
+        clientFB.addClientScheduleByClientKey(mUserId, saveItem.clientKey, clientFBItem);
+    }
+
+    /*
+     * void saveScheduleToDatabase(...) - save schedule to local database
+     */
+    private void saveScheduleToDatabase(ScheduleItem saveItem){
+        //get uri value for userSchedule table
+        Uri uriValue = ScheduleContract.CONTENT_URI;
+
+        //schedule is new, add schedule to database
+        Uri uri = mActivity.getContentResolver().insert(uriValue, saveItem.getContentValues());
+
+        if(mSaveListener != null){
+            mSaveListener.onScheduleSaved();
+        }
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Delete Schedule
+ */
+/**************************************************************************************************/
+    /*
+     * void deleteSchedule(...) - delete schedule
+     */
+    public void deleteSchedule(ScheduleItem deleteItem, OnScheduleDeletedListener listener){
+
+        //create string values used to delete schedule
+        String timestamp = deleteItem.datestamp;
+        String appDate = deleteItem.appointmentDate;
+        final String appTime = deleteItem.appointmentTime;
+        String clientKey = deleteItem.clientKey;
+        String clientName = deleteItem.clientName;
+
+        //mSaveItem = saveItem;
+        mDeleteItem = deleteItem;
+        mDeleteListener = listener;
+
+        //get schedule firebase helper instance
+        ScheduleFirebaseHelper appointmentFB = ScheduleFirebaseHelper.getInstance();
+
+        //delete schedule from firebase
+        appointmentFB.deleteSchedule(mUserId, String.valueOf(timestamp), clientName, new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    ScheduleFBItem appointment = postSnapshot.getValue(ScheduleFBItem.class);
+                    if(appointment.appointmentTime.equals(appTime)){
+                        postSnapshot.getRef().removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //does nothing
+            }
+        });
+
+        //get client schedule firebase helper
+        ClientScheduleFirebaseHelper clientFB = ClientScheduleFirebaseHelper.getInstance();
+        //delete client schedule from firebase
+        clientFB.deleteSchedule(mUserId, clientKey, appDate, appTime, new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    ClientAppFBItem appointment = postSnapshot.getValue(ClientAppFBItem.class);
+                    if(appointment.appointmentTime.equals(appTime)){
+                        postSnapshot.getRef().removeValue();
+                    }
+                }
+                deleteScheduleFromDatabase(mDeleteItem);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //does nothing
+            }
+        });
+
+    }
+
+    /*
+     * void deleteScheduleFromDatabase(...) - delete schedule data from database
+     */
+    private void deleteScheduleFromDatabase(ScheduleItem deleteItem){
+        //create string values used to delete schedule
+        String appDate = deleteItem.appointmentDate;
+        String appTime = deleteItem.appointmentTime;
+        String clientKey = deleteItem.clientKey;
+
+        //get uri value from userSchedule table
+        Uri uri = ScheduleContract.CONTENT_URI;
+
+        //remove appointment schedule from database
+        int deleted = mActivity.getContentResolver().delete(uri,
+                ScheduleQueryHelper.clientKeyDateTimeSelection,
+                new String[]{mUserId, clientKey, appDate, appTime});
+
+        if(mDeleteListener != null){
+            mDeleteListener.onScheduleDeleted();
+        }
+    }
+
+
+/**************************************************************************************************/
+
+}
