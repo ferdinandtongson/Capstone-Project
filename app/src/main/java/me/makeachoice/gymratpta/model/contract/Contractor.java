6a19127ea1ebeeb278deb06a50933c1f48d5020b
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-26 22:59:47 -0800

    Remove client note values

--- app/src/main/java/me/makeachoice/gymratpta/model/contract/Contractor.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/Contractor.java
@@ -7,7 +7,7 @@ import android.provider.BaseColumns;
 import me.makeachoice.gymratpta.model.contract.client.ClientColumns;
 import me.makeachoice.gymratpta.model.contract.client.ClientExerciseColumns;
 import me.makeachoice.gymratpta.model.contract.client.ClientRoutineColumns;
-import me.makeachoice.gymratpta.model.contract.client.NotesColumns;
+import me.makeachoice.gymratpta.model.contract.client.NotesContract;
 import me.makeachoice.gymratpta.model.contract.client.StatsColumns;
 import me.makeachoice.gymratpta.model.contract.exercise.CategoryColumns;
 import me.makeachoice.gymratpta.model.contract.exercise.ExerciseColumns;
@@ -138,82 +138,6 @@ public class Contractor {
 
     }
 
-
-    /*
-     * NotesEntry - client notes
-     */
-    public static class NotesEntry extends NotesColumns implements BaseColumns {
-
-        public static Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_NOTES).build();
-
-        public static String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTES;
-        public static String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_NOTES;
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[_id]
-        public static Uri buildNotesUri(long id) {
-            return CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).build();
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]
-        public static Uri buildNotesByUID(String uid) {
-            return CONTENT_URI.buildUpon().appendPath(uid).build();
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/client_key/[clientKey]
-        public static Uri buildNotesByClientKey(String uid, String clientKey) {
-            return CONTENT_URI.buildUpon().appendPath(uid).appendPath(COLUMN_CLIENT_KEY).appendPath(clientKey).build();
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/appointment_date/[appointmentDate]
-        public static Uri buildNotesByDate(String uid, String clientKey, String appointmentDate) {
-            return CONTENT_URI.buildUpon().appendPath(uid).appendPath(clientKey).
-                    appendPath(uid).appendPath(COLUMN_APPOINTMENT_DATE).appendPath(appointmentDate).build();
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
-        public static Uri buildNotesByDateTime(String uid, String clientKey, String appointmentDate, String appointmentTime) {
-            return CONTENT_URI.buildUpon().appendPath(uid).appendPath(clientKey).appendPath(appointmentDate).
-                    appendPath(COLUMN_APPOINTMENT_TIME).appendPath(appointmentTime).build();
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/....
-        public static String getUIdFromUri(Uri uri) {
-            return uri.getPathSegments().get(1);
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/client_key/[clientKey]
-        public static String getClientKeyFromUri(Uri uri) {
-            return uri.getPathSegments().get(3);
-        }
-
-        //content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/appointment_date/[appointmentDate]
-        public static String getClientKeyFromDateUri(Uri uri) {
-            return uri.getPathSegments().get(2);
-        }
-
-        //content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/appointment_date/[appointmentDate]
-        public static String getDateFromDateUri(Uri uri) {
-            return uri.getPathSegments().get(4);
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
-        public static String getClientKeyFromDateTimeUri(Uri uri) {
-            return uri.getPathSegments().get(2);
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
-        public static String getDateFromDateTimeUri(Uri uri) {
-            return uri.getPathSegments().get(3);
-        }
-
-        //"content://CONTENT_AUTHORITY/clientNotes/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
-        public static String getTimeFromDateTimeUri(Uri uri) {
-            return uri.getPathSegments().get(5);
-        }
-    }
 
     /*
      * StatsEntry - client stats
