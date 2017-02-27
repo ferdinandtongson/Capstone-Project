19faacb6380cf573e190a0dbf6798aca0f18e792
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-27 10:48:56 -0800

    Add appointmentDate variable to object

--- app/src/main/java/me/makeachoice/gymratpta/model/item/client/NotesFBItem.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/NotesFBItem.java
@@ -1,10 +1,9 @@
 package me.makeachoice.gymratpta.model.item.client;
 
-/**
- * Created by Usuario on 12/24/2016.
- */
 
 public class NotesFBItem {
+
+    public String appointmentDate;
     public String appointmentTime;
     public String modifiedDate;
     public String subjectiveNotes;
