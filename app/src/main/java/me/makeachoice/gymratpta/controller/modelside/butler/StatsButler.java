73f8ac6d3b3bbbcf2fc8eb60f479321de1f01470
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-02-27 19:23:18 -0800

    First commit

---
+++ app/src/main/java/me/makeachoice/gymratpta/controller/modelside/butler/StatsButler.java
@@ -0,0 +1,274 @@
+package me.makeachoice.gymratpta.controller.modelside.butler;
+
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import me.makeachoice.gymratpta.controller.modelside.firebase.client.StatsFirebaseHelper;
+import me.makeachoice.gymratpta.controller.modelside.loader.StatsLoader;
+import me.makeachoice.gymratpta.controller.modelside.query.StatsQueryHelper;
+import me.makeachoice.gymratpta.model.contract.client.StatsContract;
+import me.makeachoice.gymratpta.model.item.client.StatsFBItem;
+import me.makeachoice.gymratpta.model.item.client.StatsItem;
+import me.makeachoice.library.android.base.view.activity.MyActivity;
+
+/**************************************************************************************************/
+/*
+ *  StatsButler assists in loading, saving and deleting client stats to database and firebase
+ */
+/**************************************************************************************************/
+
+public class StatsButler {
+
+/**************************************************************************************************/
+/*
+ *  Class Variables
+ */
+/**************************************************************************************************/
+
+    //mActivity - activity context
+    private MyActivity mActivity;
+
+    //mUserId - user authentication id
+    private String mUserId;
+
+    //mSaveItem - appointment item to save to database
+    //private ScheduleItem mSaveItem;
+
+    //mDeleteItem - appointment item to delete from database
+    //private ScheduleItem mDeleteItem;
+
+    //mStatsList - list of stats loaded from database
+    private ArrayList<StatsItem> mStatsList;
+
+    //mLoaderId - loader id number
+    private int mLoaderId;
+
+    //mStatsLoader - loader class used to load stats data
+    private StatsLoader mStatsLoader;
+
+    //mLoadListener - used to listen for load events
+    private OnLoadedListener mLoadListener;
+    public interface OnLoadedListener{
+        public void onLoaded(ArrayList<StatsItem> statsList);
+    }
+
+    //mSaveListener - used to listen for save events
+    private OnSavedListener mSaveListener;
+    public interface OnSavedListener{
+        public void onSaved();
+    }
+
+    //mDeleteListener - used to listen for delete events
+    private OnDeletedListener mDeleteListener;
+    public interface OnDeletedListener{
+        public void onDeleted();
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Constructor
+ */
+/**************************************************************************************************/
+
+    public StatsButler(MyActivity activity, String userId, String clientKey){
+        mActivity = activity;
+        mUserId = userId;
+
+        mStatsList = new ArrayList<>();
+        mStatsLoader = new StatsLoader(mActivity, mUserId, clientKey);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Load Stats
+ */
+/**************************************************************************************************/
+    /*
+     * void loadStats() - load stats data from database
+     */
+    public void loadStats(int loaderId, OnLoadedListener listener){
+        mLoaderId = loaderId;
+        mLoadListener = listener;
+
+        //start loader to get stats data from database
+        mStatsLoader.loadStatsByClientKey(mLoaderId, new StatsLoader.OnStatsLoadListener() {
+            @Override
+            public void onStatsLoadFinished(Cursor cursor) {
+                onStatsLoaded(cursor);
+            }
+        });
+    }
+
+    /*
+     * void onStatsLoaded(Cursor) - data from database has been loaded
+     */
+    private void onStatsLoaded(Cursor cursor){
+        //clear list array
+        mStatsList.clear();
+
+        //get number of scheduled appointments loaded
+        int count = cursor.getCount();
+
+        //loop through cursor
+        for(int i = 0; i < count; i++){
+            //move cursor to index position
+            cursor.moveToPosition(i);
+
+            //create item from cursor data
+            StatsItem item = new StatsItem(cursor);
+
+            //add item to list
+            mStatsList.add(item);
+        }
+
+        if(mLoadListener != null){
+            mLoadListener.onLoaded(mStatsList);
+        }
+
+        //destroy loader
+        mStatsLoader.destroyLoader(mLoaderId);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Save Stats
+ */
+/**************************************************************************************************/
+
+    public void saveStats(StatsItem saveItem, OnSavedListener listener){
+        //save listener
+        mSaveListener = listener;
+
+        //save to firebase
+        saveToFirebase(saveItem);
+
+        //save to local database
+        saveToDatabase(saveItem);
+
+    }
+
+    /*
+     * void saveToStatsFB(StatsItem) - save notes to notes firebase
+     */
+    private void saveToFirebase(StatsItem saveItem){
+        //get firebase helper instance
+        StatsFirebaseHelper fbHelper = StatsFirebaseHelper.getInstance();
+
+        //create firebase item
+        StatsFBItem fbItem = new StatsFBItem();
+        fbItem.appointmentDate = saveItem.appointmentDate;
+        fbItem.appointmentTime = saveItem.appointmentTime;
+        fbItem.modifiedDate = saveItem.modifiedDate;
+        fbItem.statWeight = saveItem.statWeight;
+        fbItem.statBodyFat = saveItem.statBodyFat;
+        fbItem.statBMI = saveItem.statBMI;
+        fbItem.statNeck = saveItem.statNeck;
+        fbItem.statChest = saveItem.statChest;
+        fbItem.statRBicep = saveItem.statRBicep;
+        fbItem.statLBicep = saveItem.statLBicep;
+        fbItem.statWaist = saveItem.statWaist;
+        fbItem.statNavel = saveItem.statNavel;
+        fbItem.statHips = saveItem.statHips;
+        fbItem.statRThigh = saveItem.statRThigh;
+        fbItem.statLThigh = saveItem.statLThigh;
+        fbItem.statRCalf = saveItem.statRCalf;
+        fbItem.statLCalf = saveItem.statLCalf;
+
+        //save notes item to firebase
+        fbHelper.addStatsByTimestamp(mUserId, saveItem.clientKey, saveItem.timestamp, fbItem);
+
+    }
+
+    /*
+     * void saveToDatabase(...) - save notes to local database
+     */
+    private void saveToDatabase(StatsItem saveItem){
+        //get uri value for routine name table
+        Uri uriValue = StatsContract.CONTENT_URI;
+
+        //appointment is new, add notes to database
+        mActivity.getContentResolver().insert(uriValue, saveItem.getContentValues());
+
+        if(mSaveListener != null){
+            mSaveListener.onSaved();
+        }
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Delete Stats
+ */
+/**************************************************************************************************/
+    /*
+     * void deleteStats(...) - delete notes from firebase
+     */
+    public void deleteStats(StatsItem deleteItem, OnDeletedListener listener){
+        mDeleteListener = listener;
+
+        //create string values used to delete notes
+        String clientKey = deleteItem.clientKey;
+        String appDate = deleteItem.appointmentDate;
+        final String appTime = deleteItem.appointmentTime;
+
+        //get notes firebase helper instance
+        StatsFirebaseHelper notesFB = StatsFirebaseHelper.getInstance();
+        //delete notes from firebase
+        notesFB.deleteStats(mUserId, clientKey, appDate, appTime, new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    StatsFBItem notes = postSnapshot.getValue(StatsFBItem.class);
+                    if(notes.appointmentTime.equals(appTime)){
+                        postSnapshot.getRef().removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //does nothing
+            }
+        });
+
+        deleteStatsFromDatabase(deleteItem);
+    }
+
+    /*
+     * void deleteStatsFromDatabase(...) - delete notes data from database
+     */
+    private void deleteStatsFromDatabase(StatsItem deleteItem){
+        //create string values used to delete notes
+        String clientKey = deleteItem.clientKey;
+        String appDate = deleteItem.appointmentDate;
+        String appTime = deleteItem.appointmentTime;
+
+        //get uri value from notes table
+        Uri uri = StatsContract.CONTENT_URI;
+
+        //remove notes from database
+        mActivity.getContentResolver().delete(uri,
+                StatsQueryHelper.clientKeyDateTimeSelection,
+                new String[]{mUserId, clientKey, appDate, appTime});
+
+        if(mDeleteListener != null){
+            mDeleteListener.onDeleted();
+        }
+    }
+
+/**************************************************************************************************/
+
+}
---
+++ app/src/main/java/me/makeachoice/gymratpta/model/contract/client/StatsContract.java
@@ -0,0 +1,222 @@
+package me.makeachoice.gymratpta.model.contract.client;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import me.makeachoice.gymratpta.model.contract.MyContractor;
+
+/**************************************************************************************************/
+/*
+ *  StatsContract are columns used in stats table in the sqlite database
+ */
+/**************************************************************************************************/
+
+public class StatsContract extends MyContractor implements BaseColumns {
+
+/**************************************************************************************************/
+/*
+ *  Table and Column Variables
+ */
+/**************************************************************************************************/
+
+    // Table name
+    public static final String TABLE_NAME = "client_stats";
+
+    //user id
+    public static final String COLUMN_UID = "uid";
+
+    //client key
+    public static final String COLUMN_CLIENT_KEY = "client_key";
+
+    //timestamp
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+    //appointment date
+    public static final String COLUMN_APPOINTMENT_DATE = "appointment_date";
+
+    //appointment time
+    public static final String COLUMN_APPOINTMENT_TIME = "appointment_time";
+
+    //modified date
+    public static final String COLUMN_MODIFIED_DATE = "modified_date";
+
+    //stat weight
+    public static final String COLUMN_STAT_WEIGHT = "stat_weight";
+
+    //stat body fat
+    public static final String COLUMN_STAT_BODY_FAT = "stat_body_fat";
+
+    //stat bmi
+    public static final String COLUMN_STAT_BMI = "stat_bmi";
+
+    //stat neck
+    public static final String COLUMN_STAT_NECK = "stat_neck";
+
+    //stat chest
+    public static final String COLUMN_STAT_CHEST = "stat_chest";
+
+    //stat rbicep
+    public static final String COLUMN_STAT_RBICEP = "stat_rbicep";
+
+    //stat lbicep
+    public static final String COLUMN_STAT_LBICEP = "stat_lbicep";
+
+    //stat waist
+    public static final String COLUMN_STAT_WAIST = "stat_waist";
+
+    //stat naval
+    public static final String COLUMN_STAT_NAVEL = "stat_navel";
+
+    //stat hips
+    public static final String COLUMN_STAT_HIPS = "stat_hips";
+
+    //stat rthigh
+    public static final String COLUMN_STAT_RTHIGH = "stat_rthigh";
+
+    //stat lthigh
+    public static final String COLUMN_STAT_LTHIGH = "stat_lthigh";
+
+    //stat rcalf
+    public static final String COLUMN_STAT_RCALF = "stat_rcalf";
+
+    //stat lcalf
+    public static final String COLUMN_STAT_LCALF = "stat_lcalf";
+
+    //sort by date and time
+    public static final String SORT_ORDER_DATE_TIME = COLUMN_TIMESTAMP + " ASC, " + COLUMN_APPOINTMENT_TIME + " ASC";
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ *  Projection and Index
+ */
+ /**************************************************************************************************/
+
+    // A "projection" defines the columns that will be returned for each row
+    public static final String[] PROJECTION =
+            {
+                    COLUMN_UID,
+                    COLUMN_CLIENT_KEY,
+                    COLUMN_TIMESTAMP,
+                    COLUMN_APPOINTMENT_DATE,
+                    COLUMN_APPOINTMENT_TIME,
+                    COLUMN_MODIFIED_DATE,
+                    COLUMN_STAT_WEIGHT,
+                    COLUMN_STAT_BODY_FAT,
+                    COLUMN_STAT_BMI,
+                    COLUMN_STAT_NECK,
+                    COLUMN_STAT_CHEST,
+                    COLUMN_STAT_RBICEP,
+                    COLUMN_STAT_LBICEP,
+                    COLUMN_STAT_WAIST,
+                    COLUMN_STAT_NAVEL,
+                    COLUMN_STAT_HIPS,
+                    COLUMN_STAT_RTHIGH,
+                    COLUMN_STAT_LTHIGH,
+                    COLUMN_STAT_RCALF,
+                    COLUMN_STAT_LCALF
+            };
+
+    public static final int INDEX_UID = 0;
+    public static final int INDEX_CLIENT_KEY = 1;
+    public static final int INDEX_TIMESTAMP = 2;
+    public static final int INDEX_APPOINTMENT_DATE = 3;
+    public static final int INDEX_APPOINTMENT_TIME = 4;
+    public static final int INDEX_MODIFIED_DATE = 5;
+    public static final int INDEX_STAT_WEIGHT = 6;
+    public static final int INDEX_STAT_BODY_FAT = 7;
+    public static final int INDEX_STAT_BMI = 8;
+    public static final int INDEX_STAT_NECK = 9;
+    public static final int INDEX_STAT_CHEST = 10;
+    public static final int INDEX_STAT_RBICEP = 11;
+    public static final int INDEX_STAT_LBICEP = 12;
+    public static final int INDEX_STAT_WAIST = 13;
+    public static final int INDEX_STAT_NAVEL = 14;
+    public static final int INDEX_STAT_HIPS = 15;
+    public static final int INDEX_STAT_RTHIGH = 16;
+    public static final int INDEX_STAT_LTHIGH = 17;
+    public static final int INDEX_STAT_RCALF = 18;
+    public static final int INDEX_STAT_LCALF = 19;
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ *  Contract Entries
+ */
+/**************************************************************************************************/
+
+    public static Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_STATS).build();
+
+    public static String CONTENT_TYPE =
+            ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STATS;
+    public static String CONTENT_ITEM_TYPE =
+            ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STATS;
+
+    //"content://CONTENT_AUTHORITY/clientStats/[_id]
+    public static Uri buildStatsUri(long id) {
+        return CONTENT_URI.buildUpon().appendPath(String.valueOf(id)).build();
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]
+    public static Uri buildStatsByUID(String uid) {
+        return CONTENT_URI.buildUpon().appendPath(uid).build();
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/client_key/[clientKey]
+    public static Uri buildStatsByClientKey(String uid, String clientKey) {
+        return CONTENT_URI.buildUpon().appendPath(uid).appendPath(COLUMN_CLIENT_KEY).appendPath(clientKey).build();
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/timestamp/[timestamp]
+    public static Uri buildStatsByTimestamp(String uid, String clientKey, String timestamp) {
+        return CONTENT_URI.buildUpon().appendPath(uid).appendPath(clientKey).
+                appendPath(COLUMN_TIMESTAMP).appendPath(timestamp).build();
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
+    public static Uri buildStatsByDateTime(String uid, String clientKey, String appointmentDate, String appointmentTime) {
+        return CONTENT_URI.buildUpon().appendPath(uid).appendPath(clientKey).appendPath(appointmentDate).
+                appendPath(COLUMN_APPOINTMENT_TIME).appendPath(appointmentTime).build();
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/....
+    public static String getUIdFromUri(Uri uri) {
+        return uri.getPathSegments().get(1);
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/client_key/[clientKey]
+    public static String getClientKeyFromUri(Uri uri) {
+        return uri.getPathSegments().get(3);
+    }
+
+    //content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/timestamp/[timestamp]
+    public static String getClientKeyFromTimestampUri(Uri uri) {
+        return uri.getPathSegments().get(2);
+    }
+
+    //content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/timestamp/[timestamp]
+    public static String getTimestampFromTimestampUri(Uri uri) {
+        return uri.getPathSegments().get(4);
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
+    public static String getClientKeyFromDateTimeUri(Uri uri) {
+        return uri.getPathSegments().get(2);
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
+    public static String getDateFromDateTimeUri(Uri uri) {
+        return uri.getPathSegments().get(3);
+    }
+
+    //"content://CONTENT_AUTHORITY/clientStats/[uid]/[clientKey]/[appointmentDate]/appointment_time/[appointmentTime]
+    public static String getTimeFromDateTimeUri(Uri uri) {
+        return uri.getPathSegments().get(5);
+    }
+
+/**************************************************************************************************/
+
+}
