6884c3ecd51ce929e0deaae5607053614b128f24
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-03-04 17:53:00 -0800

    Use ClientContract values

--- app/src/main/java/me/makeachoice/gymratpta/model/item/client/ClientItem.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/client/ClientItem.java
@@ -3,8 +3,7 @@ package me.makeachoice.gymratpta.model.item.client;
 import android.content.ContentValues;
 import android.database.Cursor;
 
-import me.makeachoice.gymratpta.model.contract.Contractor;
-import me.makeachoice.gymratpta.model.contract.client.ClientColumns;
+import me.makeachoice.gymratpta.model.contract.client.ClientContract;
 
 /**************************************************************************************************/
 /*
@@ -45,16 +44,16 @@ public class ClientItem extends ClientFBItem {
     }
 
     public ClientItem(Cursor cursor){
-        uid = cursor.getString(ClientColumns.INDEX_UID);
-        fkey = cursor.getString(ClientColumns.INDEX_FKEY);
-        contactId = cursor.getLong(ClientColumns.INDEX_CONTACTS_ID);
-        clientName = cursor.getString(ClientColumns.INDEX_CLIENT_NAME);
-        email = cursor.getString(ClientColumns.INDEX_CLIENT_EMAIL);
-        phone = cursor.getString(ClientColumns.INDEX_CLIENT_PHONE);
-        firstSession = cursor.getString(ClientColumns.INDEX_FIRST_SESSION);
-        goals = cursor.getString(ClientColumns.INDEX_CLIENT_GOALS);
-        status = cursor.getString(ClientColumns.INDEX_CLIENT_STATUS);
-        profilePic = cursor.getString(ClientColumns.INDEX_PROFILE_PIC);
+        uid = cursor.getString(ClientContract.INDEX_UID);
+        fkey = cursor.getString(ClientContract.INDEX_FKEY);
+        contactId = cursor.getLong(ClientContract.INDEX_CONTACTS_ID);
+        clientName = cursor.getString(ClientContract.INDEX_CLIENT_NAME);
+        email = cursor.getString(ClientContract.INDEX_CLIENT_EMAIL);
+        phone = cursor.getString(ClientContract.INDEX_CLIENT_PHONE);
+        firstSession = cursor.getString(ClientContract.INDEX_FIRST_SESSION);
+        goals = cursor.getString(ClientContract.INDEX_CLIENT_GOALS);
+        status = cursor.getString(ClientContract.INDEX_CLIENT_STATUS);
+        profilePic = cursor.getString(ClientContract.INDEX_PROFILE_PIC);
     }
 
 /**************************************************************************************************/
@@ -72,16 +71,16 @@ public class ClientItem extends ClientFBItem {
     public ContentValues getContentValues(){
         // Add a new student record
         ContentValues values = new ContentValues();
-        values.put(Contractor.ClientEntry.COLUMN_UID, uid);
-        values.put(Contractor.ClientEntry.COLUMN_FKEY, fkey);
-        values.put(Contractor.ClientEntry.COLUMN_CONTACTS_ID, contactId);
-        values.put(Contractor.ClientEntry.COLUMN_CLIENT_NAME, clientName);
-        values.put(Contractor.ClientEntry.COLUMN_CLIENT_EMAIL, email);
-        values.put(Contractor.ClientEntry.COLUMN_CLIENT_PHONE, phone);
-        values.put(Contractor.ClientEntry.COLUMN_FIRST_SESSION, firstSession);
-        values.put(Contractor.ClientEntry.COLUMN_CLIENT_GOALS, goals);
-        values.put(Contractor.ClientEntry.COLUMN_CLIENT_STATUS, status);
-        values.put(Contractor.ClientEntry.COLUMN_PROFILE_PIC, profilePic);
+        values.put(ClientContract.COLUMN_UID, uid);
+        values.put(ClientContract.COLUMN_FKEY, fkey);
+        values.put(ClientContract.COLUMN_CONTACTS_ID, contactId);
+        values.put(ClientContract.COLUMN_CLIENT_NAME, clientName);
+        values.put(ClientContract.COLUMN_CLIENT_EMAIL, email);
+        values.put(ClientContract.COLUMN_CLIENT_PHONE, phone);
+        values.put(ClientContract.COLUMN_FIRST_SESSION, firstSession);
+        values.put(ClientContract.COLUMN_CLIENT_GOALS, goals);
+        values.put(ClientContract.COLUMN_CLIENT_STATUS, status);
+        values.put(ClientContract.COLUMN_PROFILE_PIC, profilePic);
 
         return values;
     }
