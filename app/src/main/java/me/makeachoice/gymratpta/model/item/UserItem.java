746c90a36b2e7a0630cabda7bc6da6077be72c37
Ferdinand Tongson <ferdinand.tongson@gmail.com>
2017-03-07 17:13:40 -0800

    Update Item functionality

--- app/src/main/java/me/makeachoice/gymratpta/model/item/UserItem.java
+++ app/src/main/java/me/makeachoice/gymratpta/model/item/UserItem.java
@@ -1,14 +1,76 @@
 package me.makeachoice.gymratpta.model.item;
 
-/**
- * Created by Usuario on 2/2/2017.
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import me.makeachoice.gymratpta.model.contract.user.UserContract;
+
+public class UserItem extends UserFBItem{
+
+/**************************************************************************************************/
+/*
+ * Class Variables
  */
+/**************************************************************************************************/
 
-public class UserItem {
 
-    public String uid;
-    public String userName;
-    public String email;
-    public String status;
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Constructor
+ */
+/**************************************************************************************************/
+
+    public UserItem(){}
+
+    public UserItem(UserFBItem item){
+        uid = item.uid;
+        userName = item.userName;
+        email = item.email;
+        status = item.status;
+    }
+
+    public UserItem(Cursor cursor){
+        uid = cursor.getString(UserContract.INDEX_UID);
+        userName = cursor.getString(UserContract.INDEX_USER_NAME);
+        email = cursor.getString(UserContract.INDEX_USER_EMAIL);
+        status = cursor.getString(UserContract.INDEX_USER_STATUS);
+    }
+
+/**************************************************************************************************/
+
+/**************************************************************************************************/
+/*
+ * Public Methods
+ *      ContentValues getContentValues() - convert item data to a ContentValues object
+ *      ClientItem addData(Cursor) - add cursor data to item
+ */
+    /**************************************************************************************************/
+    /*
+     * ContentValues getContentValues() - convert item data to a ContentValues object
+     */
+    public ContentValues getContentValues(){
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(UserContract.COLUMN_UID, uid);
+        values.put(UserContract.COLUMN_USER_NAME, userName);
+        values.put(UserContract.COLUMN_USER_EMAIL, email);
+        values.put(UserContract.COLUMN_USER_STATUS, status);
+
+        return values;
+    }
+
+    public UserFBItem getFBItem(){
+        UserFBItem item = new UserFBItem();
+        item.uid = uid;
+        item.userName = userName;
+        item.email = email;
+        item.status = status;
+
+        return item;
+    }
+
+/**************************************************************************************************/
 
 }
